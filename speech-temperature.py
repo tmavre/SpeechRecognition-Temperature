f558bd75c9c52b2fdacf0f4437ee92998907abf6
Thomas Mavrelis <tmavrelis@live.com>
2018-08-04 16:37:04 +0200

    add def

--- speech-temperature.py
+++ speech-temperature.py
@@ -1,38 +1,43 @@
- # import the library
-import speech_recognition as sr 
-import datetime
+import sys
+import speech_recognition as sr
 from weather import Weather, Unit
 
-while True:
-    # initialize recognizer
-    r = sr.Recognizer()  
-    # mention source it will be either Microphone or audio files.
-    with sr.Microphone() as source:  
-        print ("Speak Anything :")
-        # listen to the source
-        audio = r.listen(source)  
-        try:
-            # use recognizer to convert our audio into text part.
-            text = r.recognize_google(audio)  
-            print("You said : {}".format(text))
 
-            if text == "temperature":
-                #Get weather forecasts for the upcoming days.
-                weather = Weather (unit=Unit.CELSIUS)
-                #put your location here
-                location = weather.lookup_by_location ('brno')
-                forecasts = location.forecast
-                for forecast in forecasts:
-                    print ("===============")
-                    print ("Date: ", forecast.date)
-                    print (forecast.text)
-                    print ("high : ", forecast.high)
-                    print ("low : ", forecast.low)
-                    print ("===============")
+def start_speech_listener():
+    while True:
+        # initialize recognizer.
+        r = sr.Recognizer()
+        # mention source it will be either Microphone or audio files.
+        with sr.Microphone() as source:
+            print("Speak Anything :")
+            # listen to the source
+            audio = r.listen(source)
+            try:
+                # use recognizer to convert our audio into text part.
+                text = r.recognize_google(audio)
+                print("You said : {}".format(text))
 
-            if text == "exit":
-                break
+                if text == "temperature":
+                    # Get weather forecasts for the upcoming days.
+                    weather = Weather(unit=Unit.CELSIUS)
+                    # put your location 
+                    location = weather.lookup_by_location('brno')
+                    forecasts = location.forecast
+                    for forecast in forecasts:
+                        print("===============")
+                        print("Date: ", forecast.date)
+                        print(forecast.text)
+                        print("high : ", forecast.high)
+                        print("low : ", forecast.low)
+                        print("===============")
+                if text == "exit":
+                    sys.exit(0)
+            except sr.RequestError:
+                print(" API was unreachable or unresponsive")
+                sys.exit(1)
+            except sr.UnknownValueError:
+                print("Unknown Value Error")
 
-        except:
-            # In case of voice not recognized  clearly
-            print("Sorry could not recognize your voice")  
+
+if __name__ == '__main__':
+    start_speech_listener()
